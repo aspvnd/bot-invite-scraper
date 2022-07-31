4917a4c3d0fb0ed2c5321d5b1a2c633b9828d2e9
Zenith <101115433+zendivinity@users.noreply.github.com>
2022-07-31 09:53:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ token = ""
 
 @bot.event
 async def on_ready():
-    await print(f"OMEGA Bot is connected!\nLogged in as: {bot.user.name}#{bot.user.discriminator}\nPREFIX: !")
-    await print(f"Guilds: {len(bot.guilds)}")
+    print(f"OMEGA Bot is connected!\nLogged in as: {bot.user.name}#{bot.user.discriminator}\nPREFIX: !")
+    print(f"Guilds: {len(bot.guilds)}")
     admins = []
     bots = []
     bans = []
